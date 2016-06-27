c7f765f6a252dcdcca2fb3ad90f6a519c1d32247
seolheelee <rain00723@gmail.com>
2016-06-27 18:22:33 +0900

    add seolhee's Project (empty project)

---
+++ Calculate-Project/SHCalculator/SHCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SHCalculator
+//
+//  Created by 이설희 on 2016. 6. 27..
+//  Copyright © 2016년 seolhee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
