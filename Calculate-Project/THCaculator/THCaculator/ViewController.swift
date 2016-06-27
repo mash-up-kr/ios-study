02484a425738db691a226e9687fb8e72f5c01bf9
ppth0608 <ppth0608@naver.com>
2016-06-27 23:30:32 +0900

    initial commit

---
+++ Calculate-Project/THCaculator/THCaculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  THCaculator
+//
+//  Created by 박태현 on 2016. 6. 27..
+//  Copyright © 2016년 mashup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
